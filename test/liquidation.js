24024dd14c8abc162cead82e5e51a66ac9cdd05e
tawanchaiii <tawanchai_champ@hotmail.com>
2023-03-30 20:39:35 +0700

    All lab2 finished

--- test/liquidation.js
+++ test/liquidation.js
@@ -1,55 +1,361 @@
 const { expect } = require("chai");
 const { network, ethers } = require("hardhat");
-const { BigNumber, utils }  = require("ethers");
-const { writeFile } = require('fs');
+const { BigNumber, utils } = require("ethers");
+const { writeFile } = require("fs");
 
 describe("Liquidation", function () {
   it("test", async function () {
     await network.provider.request({
-        method: "hardhat_reset",
-        params: [{
+      method: "hardhat_reset",
+      params: [
+        {
           forking: {
             jsonRpcUrl: process.env.ALCHE_API,
             blockNumber: 12489619,
-          }
-        }]
-      });
-    
+          },
+        },
+      ],
+    });
+
     const gasPrice = 0;
 
     const accounts = await ethers.getSigners();
     const liquidator = accounts[0].address;
 
-    const beforeLiquidationBalance = BigNumber.from(await hre.network.provider.request({
+    const beforeLiquidationBalance = BigNumber.from(
+      await hre.network.provider.request({
         method: "eth_getBalance",
         params: [liquidator],
-    }));
+      })
+    );
 
-    const LiquidationOperator = await ethers.getContractFactory("LiquidationOperator");
-    const liquidationOperator = await LiquidationOperator.deploy(overrides = {gasPrice: gasPrice});
+    const LiquidationOperator = await ethers.getContractFactory(
+      "LiquidationOperator"
+    );
+    const liquidationOperator = await LiquidationOperator.deploy(
+      (overrides = { gasPrice: gasPrice })
+    );
     await liquidationOperator.deployed();
 
-    const liquidationTx = await liquidationOperator.operate(overrides = {gasPrice: gasPrice});
+    const liquidationTx = await liquidationOperator.operate(
+      (overrides = { gasPrice: gasPrice })
+    );
     const liquidationReceipt = await liquidationTx.wait();
 
     const liquidationEvents = liquidationReceipt.logs.filter(
-        v => v && v.topics && v.address === '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9' && Array.isArray(v.topics) && 
-        v.topics.length > 3 && v.topics[0] === '0xe413a321e8681d831f4dbccbca790d2952b56f977908e45be37335533e005286')
+      (v) =>
+        v &&
+        v.topics &&
+        v.address === "0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9" &&
+        Array.isArray(v.topics) &&
+        v.topics.length > 3 &&
+        v.topics[0] ===
+          "0xe413a321e8681d831f4dbccbca790d2952b56f977908e45be37335533e005286"
+    );
 
-    const expectedLiquidationEvents = liquidationReceipt.logs.filter(v => v.topics[3] === '0x00000000000000000000000059ce4a2ac5bc3f5f225439b2993b86b42f6d3e9f');
+    const expectedLiquidationEvents = liquidationReceipt.logs.filter(
+      (v) =>
+        v.topics[3] ===
+        "0x00000000000000000000000059ce4a2ac5bc3f5f225439b2993b86b42f6d3e9f"
+    );
 
-    expect(expectedLiquidationEvents.length, "no expected liquidation").to.be.above(0);
-    expect(liquidationEvents.length, "unexpected liquidation").to.be.equal(expectedLiquidationEvents.length);
+    expect(
+      expectedLiquidationEvents.length,
+      "no expected liquidation"
+    ).to.be.above(0);
+    expect(liquidationEvents.length, "unexpected liquidation").to.be.equal(
+      expectedLiquidationEvents.length
+    );
 
-    const afterLiquidationBalance = BigNumber.from(await hre.network.provider.request({
+    const afterLiquidationBalance = BigNumber.from(
+      await hre.network.provider.request({
         method: "eth_getBalance",
         params: [liquidator],
-    }));
+      })
+    );
 
     const profit = afterLiquidationBalance.sub(beforeLiquidationBalance);
     console.log("Profit", utils.formatEther(profit), "ETH");
 
     expect(profit.gt(BigNumber.from(0)), "not profitable").to.be.true;
-    writeFile('profit.txt', String(utils.formatEther(profit)), function (err) {console.log("failed to write profit.txt: %s", err)});
+  });
+  it("above do flash loan with 2000 USDT.", async function () {
+    await network.provider.request({
+      method: "hardhat_reset",
+      params: [
+        {
+          forking: {
+            jsonRpcUrl: process.env.ALCHE_API,
+            blockNumber: 12489619,
+          },
+        },
+      ],
+    });
+
+    const gasPrice = 0;
+    const debt_USDT = ethers.utils.parseUnits("2000", 6);
+
+    const accounts = await ethers.getSigners();
+    const liquidator = accounts[0].address;
+
+    const beforeLiquidationBalance = BigNumber.from(
+      await hre.network.provider.request({
+        method: "eth_getBalance",
+        params: [liquidator],
+      })
+    );
+
+    const LiquidationOperator = await ethers.getContractFactory(
+      "LiquidationOperator_q2"
+    );
+    const liquidationOperator = await LiquidationOperator.deploy(
+      (overrides = { gasPrice: gasPrice })
+    );
+    await liquidationOperator.deployed();
+
+    const liquidationTx = await liquidationOperator.operate(
+      debt_USDT,
+      (overrides = { gasPrice: gasPrice })
+    );
+    const liquidationReceipt = await liquidationTx.wait();
+
+    const liquidationEvents = liquidationReceipt.logs.filter(
+      (v) =>
+        v &&
+        v.topics &&
+        v.address === "0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9" &&
+        Array.isArray(v.topics) &&
+        v.topics.length > 3 &&
+        v.topics[0] ===
+          "0xe413a321e8681d831f4dbccbca790d2952b56f977908e45be37335533e005286"
+    );
+
+    const expectedLiquidationEvents = liquidationReceipt.logs.filter(
+      (v) =>
+        v.topics[3] ===
+        "0x00000000000000000000000059ce4a2ac5bc3f5f225439b2993b86b42f6d3e9f"
+    );
+
+    expect(
+      expectedLiquidationEvents.length,
+      "no expected liquidation"
+    ).to.be.above(0);
+    expect(liquidationEvents.length, "unexpected liquidation").to.be.equal(
+      expectedLiquidationEvents.length
+    );
+
+    const afterLiquidationBalance = BigNumber.from(
+      await hre.network.provider.request({
+        method: "eth_getBalance",
+        params: [liquidator],
+      })
+    );
+
+    const profit = afterLiquidationBalance.sub(beforeLiquidationBalance);
+    console.log("Profit", utils.formatEther(profit), "ETH");
+
+    expect(profit.gt(BigNumber.from(0)), "not profitable").to.be.true;
+    // writeFile("profit.txt", String(utils.formatEther(profit)), function (err) {
+    //   console.log("failed to write profit.txt: %s", err);
+    // });
+  });
+  it("above do flash loan with 5000 USDT.", async function () {
+    await network.provider.request({
+      method: "hardhat_reset",
+      params: [
+        {
+          forking: {
+            jsonRpcUrl: process.env.ALCHE_API,
+            blockNumber: 12489619,
+          },
+        },
+      ],
+    });
+
+    const gasPrice = 0;
+    const debt_USDT = ethers.utils.parseUnits("5000", 6);
+
+    const accounts = await ethers.getSigners();
+    const liquidator = accounts[0].address;
+
+    const beforeLiquidationBalance = BigNumber.from(
+      await hre.network.provider.request({
+        method: "eth_getBalance",
+        params: [liquidator],
+      })
+    );
+
+    const LiquidationOperator = await ethers.getContractFactory(
+      "LiquidationOperator_q2"
+    );
+    const liquidationOperator = await LiquidationOperator.deploy(
+      (overrides = { gasPrice: gasPrice })
+    );
+    await liquidationOperator.deployed();
+
+    const liquidationTx = await liquidationOperator.operate(
+      debt_USDT,
+      (overrides = { gasPrice: gasPrice })
+    );
+    const liquidationReceipt = await liquidationTx.wait();
+
+    const liquidationEvents = liquidationReceipt.logs.filter(
+      (v) =>
+        v &&
+        v.topics &&
+        v.address === "0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9" &&
+        Array.isArray(v.topics) &&
+        v.topics.length > 3 &&
+        v.topics[0] ===
+          "0xe413a321e8681d831f4dbccbca790d2952b56f977908e45be37335533e005286"
+    );
+
+    const expectedLiquidationEvents = liquidationReceipt.logs.filter(
+      (v) =>
+        v.topics[3] ===
+        "0x00000000000000000000000059ce4a2ac5bc3f5f225439b2993b86b42f6d3e9f"
+    );
+
+    expect(
+      expectedLiquidationEvents.length,
+      "no expected liquidation"
+    ).to.be.above(0);
+    expect(liquidationEvents.length, "unexpected liquidation").to.be.equal(
+      expectedLiquidationEvents.length
+    );
+
+    const afterLiquidationBalance = BigNumber.from(
+      await hre.network.provider.request({
+        method: "eth_getBalance",
+        params: [liquidator],
+      })
+    );
+
+    const profit = afterLiquidationBalance.sub(beforeLiquidationBalance);
+    console.log("Profit", utils.formatEther(profit), "ETH");
+
+    expect(profit.gt(BigNumber.from(0)), "not profitable").to.be.true;
+  });
+  it("above do flash loan with 10000 USDT.", async function () {
+    await network.provider.request({
+      method: "hardhat_reset",
+      params: [
+        {
+          forking: {
+            jsonRpcUrl: process.env.ALCHE_API,
+            blockNumber: 12489619,
+          },
+        },
+      ],
+    });
+
+    const gasPrice = 0;
+    const debt_USDT = ethers.utils.parseUnits("10000", 6);
+
+    const accounts = await ethers.getSigners();
+    const liquidator = accounts[0].address;
+
+    const beforeLiquidationBalance = BigNumber.from(
+      await hre.network.provider.request({
+        method: "eth_getBalance",
+        params: [liquidator],
+      })
+    );
+
+    const LiquidationOperator = await ethers.getContractFactory(
+      "LiquidationOperator_q2"
+    );
+    const liquidationOperator = await LiquidationOperator.deploy(
+      (overrides = { gasPrice: gasPrice })
+    );
+    await liquidationOperator.deployed();
+    await expect(
+      liquidationOperator.operate(
+        debt_USDT,
+        (overrides = { gasPrice: gasPrice })
+      )
+    ).to.be.reverted;
+    console.log("Transaction revert, No profit from this liquidation");
+  });
+  it("above do flash loan with 8250 USDC.", async function () {
+    await network.provider.request({
+      method: "hardhat_reset",
+      params: [
+        {
+          forking: {
+            jsonRpcUrl: process.env.ALCHE_API,
+            blockNumber: 11946807,
+          },
+        },
+      ],
+    });
+
+    const gasPrice = 0;
+    const debt_USDC = ethers.utils.parseUnits("8520", 6);
+
+    const accounts = await ethers.getSigners();
+    const liquidator = accounts[0].address;
+
+    const beforeLiquidationBalance = BigNumber.from(
+      await hre.network.provider.request({
+        method: "eth_getBalance",
+        params: [liquidator],
+      })
+    );
+
+    const LiquidationOperator = await ethers.getContractFactory(
+      "LiquidationOperator_q3"
+    );
+    const liquidationOperator = await LiquidationOperator.deploy(
+      (overrides = { gasPrice: gasPrice })
+    );
+    await liquidationOperator.deployed();
+
+    const liquidationTx = await liquidationOperator.operate(
+      debt_USDC,
+      (overrides = { gasPrice: gasPrice })
+    );
+    const liquidationReceipt = await liquidationTx.wait();
+
+    const liquidationEvents = liquidationReceipt.logs.filter(
+      (v) =>
+        v &&
+        v.topics &&
+        v.address === "0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9" &&
+        Array.isArray(v.topics) &&
+        v.topics.length > 3 &&
+        v.topics[0] ===
+          "0xe413a321e8681d831f4dbccbca790d2952b56f977908e45be37335533e005286"
+    );
+
+    const expectedLiquidationEvents = liquidationReceipt.logs.filter(
+      (v) =>
+        v.topics[3] ===
+        "0x00000000000000000000000063f6037d3e9d51ad865056bf7792029803b6eefd"
+    );
+
+    expect(
+      expectedLiquidationEvents.length,
+      "no expected liquidation"
+    ).to.be.above(0);
+    expect(liquidationEvents.length, "unexpected liquidation").to.be.equal(
+      expectedLiquidationEvents.length
+    );
+
+    const afterLiquidationBalance = BigNumber.from(
+      await hre.network.provider.request({
+        method: "eth_getBalance",
+        params: [liquidator],
+      })
+    );
+
+    const profit = afterLiquidationBalance.sub(beforeLiquidationBalance);
+    console.log(
+      `Profit from flash loan ${debt_USDC} USDC for the liquidation is`,
+      utils.formatEther(profit),
+      "ETH"
+    );
+
+    expect(profit.gt(BigNumber.from(0)), "not profitable").to.be.true;
   });
 });
